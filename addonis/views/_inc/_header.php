ea79c4ebb74f872cc06963d68d7b48fd013d4437
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-12 12:30:11 +0000

    Implement basic styling with Twitter Bootstrap.
    Butcher HTML in the process.

--- addonis/views/_inc/_header.php
+++ addonis/views/_inc/_header.php
@@ -3,7 +3,22 @@
 <head>
   <meta charset="utf-8" />
   <title><?php echo $meta_title; ?></title>
+
+  <link href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css"
+    rel="stylesheet" />
+  <link href="/css/app.css" rel="stylesheet" />
 </head>
 
 <body>
+<div class="topbar">
+  <div class="topbar-inner">
+    <div class="container">
+      <a class="brand" href="/">Addonis</a>
 
+      <ul class="nav">
+        <li><a href="/package/" title="Create a new add-on package">Package Builder</a></li>
+        <li><a href="/datatype/" title="Create a new datatype">Datatype Builder</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
--- addonis/views/build/package.php
+++ addonis/views/build/package.php
@@ -1,177 +1,245 @@
+<div class="container">
+
 <h1>Package Builder</h1>
-<p>The Addonis package builder.</p>
 
 <form>
   <!-- Package Information -->
   <fieldset>
     <legend>Package Information</legend>
 
-    <div class="fld">
+    <div class="clearfix">
       <label for="pkg_name">Name</label>
-      <input id="pkg_name" name="pkg_name" type="text" />
+      <div class="input"><input id="pkg_name" name="pkg_name" type="text" /></div>
     </div>
 
-    <div class="fld">
+    <div class="clearfix">
       <label for="pkg_short_name">Short Name</label>
-      <input id="pkg_short_name" name="pkg_short_name" type="text" />
+      <div class="input"><input id="pkg_short_name" name="pkg_short_name" type="text" /></div>
     </div>
 
-    <div class="fld">
+    <div class="clearfix">
       <label for="pkg_description">Description</label>
-      <input id="pkg_description" name="pkg_description" type="text" />
+      <div class="input"><input id="pkg_description" name="pkg_description" type="text" /></div>
     </div>
 
-    <div class="fld">
+    <div class="clearfix">
       <label for="pkg_version">Version</label>
-      <input id="pkg_version" name="pkg_version" type="text" />
+      <div class="input"><input id="pkg_version" name="pkg_version" type="text" /></div>
     </div>
 
-    <div class="fld">
+    <div class="clearfix">
       <label for="pkg_license">License</label>
-      <select id="pkg_license" name="pkg_license">
-        <option value="client">Client</option>
-        <option value="commercial">Commercial</option>
-        <option value="free">Free</option>
-      </select>
+      <div class="input">
+        <select id="pkg_license" name="pkg_license">
+          <option value="client">Client</option>
+          <option value="commercial">Commercial</option>
+          <option value="free">Free</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <label for="acc_include">Include Accessory?</label>
+      <div class="input">
+        <select id="acc_include" name="acc_include">
+          <option value="y">Yes</option>
+          <option selected="selected" value="n">No</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <label for="ext_include">Include Extension?</label>
+      <div class="input">
+        <select id="ext_include" name="ext_include">
+          <option value="y">Yes</option>
+          <option selected="selected" value="n">No</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <label for="ft_include">Include Fieldtype?</label>
+      <div class="input">
+        <select id="ft_include" name="ft_include">
+          <option value="y">Yes</option>
+          <option selected="selected" value="n">No</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <label for="mod_include">Include Module?</label>
+      <div class="input">
+        <select id="mod_include" name="mod_include">
+          <option value="y">Yes</option>
+          <option selected="selected" value="n">No</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <label for="pi_include">Include Plugin?</label>
+      <div class="input">
+        <select id="pi_include" name="pi_include">
+          <option value="y">Yes</option>
+          <option selected="selected" value="n">No</option>
+        </select>
+      </div>
     </div>
   </fieldset>
+
+  <hr />
 
   <!-- Accessory -->
   <fieldset>
     <legend>Accessory Information</legend>
-
-    <div class="fld">
-      <label><input id="acc_include" name="acc_include" type="checkbox"
-        value="y" /> Include Accessory?</label>
-    </div>
   </fieldset>
+
+  <hr />
 
   <!-- Extension -->
   <fieldset>
     <legend>Extension Information</legend>
-
-    <div class="fld">
-      <label><input id="ext_include" name="ext_include" type="checkbox"
-        value="y" /> Include Extension?</label>
-    </div>
   </fieldset>
+
+  <hr />
 
   <!-- Fieldtype -->
   <fieldset>
     <legend>Fieldtype Information</legend>
-
-    <div class="fld">
-      <label><input id="ft_include" name="ft_include" type="checkbox"
-        value="y" /> Include Fieldtype?</label>
-    </div>
   </fieldset>
+
+  <hr />
 
   <!-- Module -->
   <fieldset>
     <legend>Module Information</legend>
 
-    <div class="fld">
-      <label><input id="mod_include" name="mod_include" type="checkbox"
-        value="y" /> Include Module?</label>
-    </div>
-
-    <div class="fld">
-      <label><input id="mod_cp" name="mod_cp" type="checkbox" value="y" />
-        Module has control panel?</label>
+    <div class="clearfix">
+      <label for="mod_cp">Module has Control Panel?</label>
+      <div class="input">
+        <select id="mod_cp" name="mod_cp">
+          <option selected="selected" value="y">Yes</option>
+          <option value="n">No</option>
+        </select>
+      </div>
     </div>
 
     <!-- Module : Actions -->
-    <fieldset>
-      <legend>ACTions</legend>
+    <div class="clearfix">
+      <label>Actions</label>
 
-      <table>
-        <thead>
-          <tr>
-          <th scope="col">Method</th>
-          <th scope="col">Description</th>
-          </tr>
-        </thead>
+      <div class="input">
+        <table class="bordered-table condensed zebra-striped">
+          <thead>
+            <tr>
+            <th scope="col">&nbsp;</th>
+            <th scope="col">Method</th>
+            <th scope="col">Description</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-          <td><input name="mod_actions[0]['method']" type="text" /></td>
-          <td><input name="mod_actions[0]['description']" type="text" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </fieldset>
+          <tbody>
+            <tr>
+            <td>+ / -</td>
+            <td><input name="mod_actions[0]['method']" type="text" /></td>
+            <td><input name="mod_actions[0]['description']" type="text" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
     <!-- Module : Template Tags -->
-    <fieldset>
-      <legend>Template Tags</legend>
+    <div class="clearfix">
+      <label>Template Tags</label>
 
-      <table>
-        <thead>
-          <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          </tr>
-        </thead>
+      <div class="input">
+        <table class="bordered-table condensed zebra-striped">
+          <thead>
+            <tr>
+            <th scope="col">&nbsp;</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-          <td><input name="mod_tags[0]['name']" type="text" /></td>
-          <td><input name="mod_tags[0]['description']" type="text" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </fieldset>
-
+          <tbody>
+            <tr>
+            <td>+ / -</td>
+            <td><input name="mod_tags[0]['name']" type="text" /></td>
+            <td><input name="mod_tags[0]['description']" type="text" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </fieldset>
+
+  <hr />
 
   <!-- Plugin -->
   <fieldset>
     <legend>Plugin Information</legend>
-
-    <div class="fld">
-      <label><input id="pi_include" name="pi_include" type="checkbox"
-        value="y" /> Include Plugin?</label>
-    </div>
   </fieldset>
+
+  <hr />
 
   <!-- Extras -->
   <fieldset>
     <legend>Extras</legend>
 
     <!-- Extras : Datatypes -->
-    <fieldset>
-      <legend>Datatypes</legend>
-
-      <div class="fld">
-        <label><input name="extras_datatypes[]" type="checkbox" value="ee_member_group" />
-          EE Member Group</label>
+    <div class="clearfix">
+      <label for="extras_datatypes">Datatypes</label>
+      <div class="input">
+        <ul class="inputs-list">
+          <li>
+            <label><input name="extras_datatypes" type="checkbox" value="ee_member" />
+              <span>EE Member</span></label>
+          </li>
+          <li>
+            <label><input name="extras_datatypes" type="checkbox" value="ee_member_group" />
+              <span>EE Member Group</span></label>
+          </li>
+        </ul>
       </div>
-    </fieldset>
+    </div>
 
     <!-- Extras : Helpers -->
-    <fieldset>
-      <legend>Helpers</legend>
-
-      <div class="fld">
-        <label><input name="extras_helpers[]" type="checkbox" value="number" />
-          Number</label>
+    <div class="clearfix">
+      <label for="extras_helpers">Helpers</label>
+      <div class="input">
+        <ul class="inputs-list">
+          <li>
+            <label><input name="extras_helpers" type="checkbox" value="number" />
+              <span>Number</span></label>
+          </li>
+        </ul>
       </div>
-    </fieldset>
+    </div>
 
     <!-- Extras : Libraries -->
-    <fieldset>
-      <legend>Libraries</legend>
-
-      <div class="fld">
-        <label><input name="extras_libraries[]" type="checkbox" value="salesforce" />
-          SalesForce</label>
+    <div class="clearfix">
+      <label for="extras_libs">Libraries</label>
+      <div class="input">
+        <ul class="inputs-list">
+          <li>
+            <label><input name="extras_libs" type="checkbox" value="salesforce" />
+              <span>SalesForce</span></label>
+          </li>
+        </ul>
       </div>
-    </fieldset>
+    </div>
   </fieldset>
 
   <!-- All done -->
-  <div class="fld fld_submit">
-    <input type="submit" value="Create Package" />
-    <i>or <input type="reset" value="Reset the form" /></i>
+  <div class="actions">
+    <input class="btn large primary" type="submit" value="Create Package" />
+    &nbsp;&nbsp;or&nbsp;&nbsp;
+    <button class="btn" type="reset">Reset the form</button>
   </div>
 </form>
+
+</div>
