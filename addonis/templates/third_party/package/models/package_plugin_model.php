8de44505b0a579e44146f428c1c3d4f5dc47d203
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-13 16:03:43 +0000

    Create bare-bones plugin model.

---
+++ addonis/templates/third_party/package/models/package_plugin_model.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('Direct script access not allowed');
+
+/**
+ * {pkg_title} 'Plugin' model.
+ *
+ * @author          Stephen Lewis (http://github.com/experience/)
+ * @copyright       Experience Internet
+ * @package         {pkg_name}
+ */
+
+class {pkg_name}_plugin_model extends CI_Model {
+
+  private $EE;
+
+
+  /* --------------------------------------------------------------
+  * PUBLIC METHODS
+  * ------------------------------------------------------------ */
+
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+    $this->EE =& get_instance();
+  }
+
+
+}
+
+
+/* End of file      : {pkg_name_lc}_plugin_model.php */
+/* File location    : third_party/{pkg_name_lc}/models/{pkg_name_lc}_plugin_model.php */
