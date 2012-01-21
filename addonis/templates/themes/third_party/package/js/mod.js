fdea4b9119fc30dec9ed93e9d6958264f49509c6
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-21 08:57:44 +0000

    Add placeholder CSS and JS files.

---
+++ addonis/templates/themes/third_party/package/js/mod.js
@@ -0,0 +1,18 @@
+/**
+ * {pkg_title} module-specific JavaScript.
+ *
+ * @author        Stephen Lewis
+ * @package       {pkg_name}
+ */
+
+(function($) {
+
+  $(document).ready(function() {
+    console.log('Running the {pkg_title} Module JavaScript.');
+  });
+
+})(jQuery);
+
+
+/* End of file    : acc.js */
+/* File location  : themes/third_party/{pkg_name_lc}/js/mod.js */
